572a643cbbf04fa17abb3a0ba1e489f12ef0b5e1
Taska Tamas <tamas.taska@gmx.com>
2023-04-09 12:52:16 +0200

    Fixed exception thrown in frontend

--- frontend/src/httpclient/configresult.ts
+++ frontend/src/httpclient/configresult.ts
@@ -2,19 +2,27 @@ import { Side } from "../cube/side";
 import { Error } from "../error/error";
 
 export class ConfigResult {
-    colorPalette: Map<Side, string[][]>;
+    colorPalette: Map<Side, string[][]> = new Map();
     errors: Error[];
     constructor(data: any) {
-        this.colorPalette = new Map();
-        const stringToColors: Map<string, string[][]> = new Map(Object.entries(data.data.sides));
-        stringToColors.forEach((colors, key) => {
-            const side = Side[key as unknown as keyof typeof Side];
-            this.colorPalette.set(side, colors);
-        });
+        this.createEmptyPalette();
+        if (data.data) {
+            const stringToColors: Map<string, string[][]> = new Map(Object.entries(data.data.sides));
+            stringToColors.forEach((colors, key) => {
+                const side = Side[key as unknown as keyof typeof Side];
+                this.colorPalette.set(side, colors);
+            });
+        }
         if (data.errors) {
             this.errors = data.errors.map((error: any) => new Error(error));
         } else {
             this.errors = [];
         }
+    }
+
+    createEmptyPalette() {
+        const sides = [Side.Front, Side.Back, Side.Left, Side.Right, Side.Up, Side.Down];
+        const emptySide = Array(3).fill(Array(3).fill("-"));
+        this.colorPalette = new Map(sides.map(side => [side, emptySide]));
     }
 }
